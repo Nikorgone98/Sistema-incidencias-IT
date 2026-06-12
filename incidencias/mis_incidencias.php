57aa99d72dd3cbaa4e3f45d5e9a3d8939a03f065
Nikorgone98 <nicolasgarciaconesa@gmail.com>
2026-06-12 20:06:14 +0200

    Create mis_incidencias.php
    Permite a cada usuario consultar únicamente las incidencias que ha creado, mostrando su estado actual y proporcionando acceso al detalle de cada una de ellas.

---
+++ incidencias/mis_incidencias.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once '../includes/auth.php';
+require_once '../config/database.php';
+
+$sql = " SELECT
+	i.id_incidencia,
+	i.titulo,
+	i.fecha_creacion,
+	e.nombre_estado,
+	p.nombre_prioridad
+FROM incidencias i
+	INNER JOIN estados e
+		ON i.id_estado = e.id_estado
+	INNER JOIN prioridades p
+		ON i.id_prioridad = p.id_prioridad
+	WHERE i.id_usuario = :usuario
+	ORDER BY i.fecha_creacion DESC
+	";
+
+$stmt = $conexion->prepare($sql);
+
+$stmt->execute(['usuario'=> $_SESSION['usuario_id']]);
+
+$incidencias = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<link rel="stylesheet" href="../assets/css/style.css">
+<meta charset="UTF-8">
+<title>Mis incidencias</title>
+</head>
+
+<body>
+<h1>Mis incidencias</h1>
+
+<table class="ticket-table">
+
+<tr>
+	<th>ID</th>
+	<th>Título</th>
+	<th>Estado</th>
+	<th>Prioridad</th>
+	<th>Fecha</th>
+</tr>
+
+<?php foreach($incidencias as $incidencia): ?>
+
+<tr>
+	<td>
+	<span class="ticket-id">
+		<?php echo $incidencia['id_incidencia']; ?>
+	</span>
+	</td>
+
+	<td>
+		<a href="ver.php?id=<?php echo $incidencia['id_incidencia']; ?>">
+		<?php echo $incidencia['titulo']; ?>
+	</td>
+
+	<td> <?php $estadoClase = strtolower($incidencia['nombre_estado']);
+		if ($estadoClase == 'en curso') {
+		    $estadoClase = 'curso';
+		}?>
+		<span class="estado estado-<?php echo $estadoClase; ?>">
+		<?php echo htmlspecialchars($incidencia['nombre_estado']); ?>
+		</span>
+	</td>
+
+	<td> <?php $prioridadClase = strtolower($incidencia['nombre_prioridad']);
+		if ($prioridadClase == 'crítica') {
+		    $prioridadClase = 'critica';
+		}?>
+		<span class="priority-dot priority-<?php echo $prioridadClase; ?>"></span>
+		<?php echo htmlspecialchars($incidencia['nombre_prioridad']); ?>
+	</td>
+
+	<td> <?php echo $incidencia['fecha_creacion']; ?> </td>
+
+</tr>
+
+<?php endforeach; ?>
+
+</table>
+<br>
+
+<a href="../dashboard.php"> Volver al panel</a>
+</body>
+</html>
