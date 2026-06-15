7aa85edabac6a77a2102e96a531c8e60609af846
Nikorgone98 <nicolasgarciaconesa@gmail.com>
2026-06-15 20:25:26 +0200

    logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
-
+/* Inicia la sesión actual */
 session_start();
-
+/* Destruye la sesión y cerrar la autenticación del usuario */
 session_destroy();
-
+/* Redirige al usuario a la página de login */
 header(
 	"Location: login.php"
 	);
